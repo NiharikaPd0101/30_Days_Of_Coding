e7da56cef861e5cead383328827d99e8565b836c
NiharikaPd0101 <80149490+NiharikaPd0101@users.noreply.github.com>
2021-06-11 00:22:45 +0530

    Create Solution1.java

---
+++ Solution1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ques1
+{
+   static int total(int num)
+    {
+        if (num == 1) 
+        	return 0;
+        else if (num == 2) 
+        	return 1;
+        else
+            return (num - 1) * (total(num- 1) +  total(num - 2));
+                         
+    }
+
+    public static void main (String[] args)
+    {
+    	Scanner in=new Scanner(System.in);
+    	System.out.println("Enter a positive integer");
+        int n =in.nextInt();
+        System.out.println( "the number of elements such hat no element appears in its original position " +total(n));                     
+    }
+    }
+    
+    
