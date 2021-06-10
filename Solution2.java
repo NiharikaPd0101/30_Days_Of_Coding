dc58a9a6075b3b97fd4aae7fb3bd978f18c32565
NiharikaPd0101 <80149490+NiharikaPd0101@users.noreply.github.com>
2021-06-11 01:15:08 +0530

    Create Solution2.java

---
+++ Solution2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+ 
+public class Ques2 {
+    public static void main(String args[]) {
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("Enter the number");
+        int num = sc.nextInt();
+        System.out.println("The required series is:");
+        int a=0;
+        int b=1;
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+        for(int i=0; i<num-1; i++) {
+        	int c=a+b;
+        	System.out.print(c+" ");
+        	a=b;
+        	b=c;
+        }
+        System.out.println("and");
+        System.out.println("Partial sum of Fibonacci numbers is ");
+        System.out.println( result(num));
+        
+    }
+    
+    static int result(int num)
+    {
+
+        int fi_no[]=new int[num+1];
+        fi_no[0] = 0; 
+        fi_no[1] = 1;
+        int add = fi_no[0] + fi_no[1];
+      
+        for (int i=2; i<=num; i++)
+        {
+        	//FORMULA: fn = fn-1 + fn-2
+        	fi_no[i] = fi_no[i-1]+fi_no[i-2];
+            add += fi_no[i];
+        }
+      
+        return add;
+    }
+}
