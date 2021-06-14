04b5e21631c86ec1f5e62cf488ca1e5094730cb1
NiharikaPd0101 <80149490+NiharikaPd0101@users.noreply.github.com>
2021-06-14 15:56:12 +0530

    Create Solution75.java

---
+++ Solution75.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class Ques75 {
+ 
+    static long D_fac(long n)
+    {
+        if (n == 0 || n==1)
+            return 1;
+             
+        return n * D_fac(n - 2);
+    }
+ 
+    
+     public static void main (String[] args)
+    {
+    	 Scanner sc=new Scanner(System.in);
+    	 System.out.println("Enter the number");
+    	 int n= sc.nextInt();
+        System.out.println("Double factorial is "+ D_fac(n));
+            
+    }
+}
