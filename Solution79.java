7ddc536ff1ba8de15fca0c8eabc91f3bd2a37f00
NiharikaPd0101 <80149490+NiharikaPd0101@users.noreply.github.com>
2021-06-14 15:56:58 +0530

    Create Solution79.java

---
+++ Solution79.java
@@ -0,0 +1,16 @@
+public class Ques79
+{
+ public static void main(String[] args) 
+ {
+   System.out.print("Enter number of rows: ");
+   int rows = 4;
+         int k=1;
+        System.out.println("Here is your pattern....!!!");
+        for (int i = 1; i <= 4; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            	System.out.print(k++ +" ");
+            System.out.println(" ");
+        }  
+    }
+}
