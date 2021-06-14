9b349bd76576488f72263ece710e77688f8cdfac
NiharikaPd0101 <80149490+NiharikaPd0101@users.noreply.github.com>
2021-06-14 16:21:47 +0530

    Create Solution85.java

---
+++ Solution85.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Ques85 
+{  
+      public static void main(String args[])  
+       {  
+    	  Scanner sc=new Scanner(System.in);
+    	  System.out.println("Enter the number of rows");
+          int row=  sc.nextInt();
+            for (int i= row; i>= 1; i--)  
+              {  
+                  for (int j=row; j>i;j--)  
+                   {  
+                      System.out.print(" ");  
+                   }  
+                   for (int k=1;k<=i;k++)  
+                     {  
+                        System.out.print(k);
+                     }  
+                  System.out.println("");  
+              }
+       }
+}
